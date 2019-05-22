d80bbf0cbd10812e2649c3855f19482cf04d2786
Mikolaj Musielak <mikolaj.musielak@tedd.berlin>
2019-05-22 02:28:53 +0200

    better structure of folder content

---
+++ src/core/parse.js
@@ -0,0 +1,85 @@
+/*
+chcp 65001
+dir x: /s/a/tc > foo.txt
+*/
+
+const node = {
+  os: require("os"),
+  fs: require("fs"),
+  path: require("path")
+};
+
+var list = [].concat(
+  node.fs.readFileSync("in/v.txt").toString().split(node.os.EOL),
+  node.fs.readFileSync("in/x.txt").toString().split(node.os.EOL),
+  node.fs.readFileSync("in/y.txt").toString().split(node.os.EOL));
+
+console.log('>', list.length);
+
+var entries = [];
+var dir;
+var current;
+
+while (list.length) {
+  var re, line = list.shift();
+
+  if (line == "") {
+    continue;
+  }
+  else if (re = line.match(/^ Directory of (.+)$/)) {
+    
+    dir = re[1].replace(/\\/g, '/');
+    var info = analyze(dir);
+    
+    if (info) {
+      current = info;
+    
+      var check = entries.find(
+        (item) => {
+          return item.meta.title == info.meta.title
+            && item.meta.release == info.meta.release
+        });
+
+      if (check) {
+        console.log('DUP', check.files.location, info.files.location);
+      }
+      else {
+        entries.push(info);
+      }
+    }
+  }
+  else if (re = line.match(/^(\d\d\.\d\d.\d\d\d\d  \d\d\:\d\d)\s+((?:\d{1,3}\s)+)(.+)$/)) {
+    if (! re[3].match(/^<DIR>/)) {
+      if (current) {
+        current.files.content.push({
+          name: (dir + '/' + re[3]).substr(current.files.location.length + 1),
+          time: convertDate(re[1]),
+          size: Number(re[2].replace(/\s/g, "")) // remove whitespace
+        });
+      }
+    }
+  }
+}
+
+node.fs.writeFileSync("out/list.json", JSON.stringify(entries, null, 2));
+console.log('<', entries.length);
+
+function analyze(path) {
+  var result = node.path.basename(path).match(/^(.+) \((.+), (\d{4})\)$/);
+
+  return result && {
+    files: {
+      content: [],
+      location: path
+    },
+    meta: {
+      title: result[1].trim(),
+      directors: result[2].trim(),
+      release: result[3].trim()
+    }
+  };
+}
+
+function convertDate(dos) {
+  return `${dos.substr(6, 4)}-${dos.substr(3, 2)}-${dos.substr(0, 2)}`;
+}

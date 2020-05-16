8181c80a765379b47b09dc69f0d957ab1872f815
Mikolaj Musielak <mikolaj.musielak@ergo.digital>
2020-05-16 23:43:54 +0200

    wip

---
+++ src/core/fetch.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const https = require("https");
+
+function request(url) {
+  return new Promise((resolve, reject) => {
+    (url.startsWith("https") ? https : http)
+    .get(url, (res) => {
+      if (res.statusCode !== 200) {
+        res.resume();
+        reject(res.statusCode);
+      } else {
+        var chunks = "";
+        res.setEncoding("utf8");
+        res.on("data", (chunk) => {
+          chunks += (chunk);
+        });
+        res.on("end", () => {
+          resolve(chunks);
+        });
+      }
+    })
+    .on("error", (error) => {
+      reject(error);
+    });
+  });
+}
